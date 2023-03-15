b009759fe89f390fdc217a946eba9fa62f3c090e
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2023-03-15 10:47:15 +0300

    work with user

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,35 @@
-const db = require("../db");
+const knex = require("../db");
+
+const fs = require("fs");
+const path = require("path");
+const csv = require("csv-parser");
+
+// Первый вариант
+
+const dateDb = require("../dataDb.json");
+const parseDate = JSON.parse(JSON.stringify(dateDb));
+const popularPeople = parseDate.filter((element) => {
+  if (element.likes > 0) {
+    return console.log(`Вы популярный пользователь ${element.id}`);
+  } else {
+    console.log("Набирайте лайки");
+  }
+});
+
+// const likesJson = JSON.stringify(popularPeople);
+// console.log(likesJson);
+
+// Второй вариант///////////////////////
+// const results = [];
+
+// fs.createReadStream("articles.csv")
+//   .pipe(csv())
+//   .on("data", (data) => results.push(data))
+//   .on("end", () => {
+//     console.log("ок");
+//   });
+
+// console.log(results);
 
 class ArticleController {
   async createArticle(req, res) {
--- db.js
+++ db.js
@@ -1,10 +1,29 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
+const Knex = require("knex");
+
+// const knexFile = require("./knexfile");
+// const db = knex(knexFile.development);
+const connection = {
+  ssl: { rejectUnauthorized: false },
+
   user: "postgres",
   password: "100Mamoge",
   host: "localhost",
   port: "5432",
   database: "node_postgres",
+};
+const knex = Knex({
+  client: "pg",
+  connection,
 });
+module.exports = knex;
 
-module.exports = pool;
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "100Mamoge",
+//   host: "localhost",
+//   port: "5432",
+//   database: "node_postgres",
+// });
+
+// module.exports = pool;
