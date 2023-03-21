f7e7258ddfd056f0239726c10339c697231072f7
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2023-03-21 12:02:47 +0300

    getAutoByQuery

--- controller/controller.js
+++ controller/controller.js
@@ -35,8 +35,44 @@ class ArticleController {
             rest.transmission === "" ? null : Number(rest.transmission),
         };
       });
+
       await knex("articles").insert(insertData);
+
       res.json("ок");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async getAutoByQuery(req, res) {
+    try {
+      const { carId } = req.params;
+      const { sort, engine, transmission, wheeldrive, about } = req.query;
+      console.log(sort, carId, engine, transmission, wheeldrive, about);
+      const query = await knex("articles")
+        .select("*")
+        .where({ generationId: carId });
+      if (sort === "date") {
+        query.orderBy("date");
+      } else if (sort === "comments") {
+        query.orderBy("comments");
+      } else if (sort === "likes") {
+        query.orderBy("likes");
+      }
+
+      if (engine >= 0 && engine <= 4) {
+        query.andWhere({ engine });
+      }
+
+      if (transmission >= 0 && transmission <= 4) {
+        query.andWhere({ transmission });
+      }
+
+      if (wheeldrive >= 0 && wheeldrive <= 2) {
+        query.andWhere({ wheeldrive });
+      }
+
+      res.json(query);
     } catch (err) {
       console.log(err);
     }
--- routes/routeApp.js
+++ routes/routeApp.js
@@ -2,6 +2,7 @@ const Router = require("express");
 const router = new Router();
 const articleController = require("../controller/controller");
 
-router.get("/articles", articleController.createArticle);
+// router.get("/articles", articleController.createArticle);
+router.get("/model/:carId", articleController.getAutoByQuery);
 
 module.exports = router;
